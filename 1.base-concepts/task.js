b7ca7652e01ffd1f68592aa2c30df6ca8dde8d67
YurinaQA <158807207+YurinaQA@users.noreply.github.com>
2024-08-06 08:54:51 +0500

    task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = (b**2-4*a*c);
+  if (d>0){
+   let root1 = (-b + Math.sqrt(d) )/(2*a);
+   let root2 =  (-b - Math.sqrt(d) )/(2*a);
+   arr.push(root1,root2);
+   return arr;
+  }
+
+if (d === 0){
+  let root0= -b/(2*a);
+  arr.push(root0);
+  }
   return arr;
 }
 
